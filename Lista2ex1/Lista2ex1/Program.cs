c025dac6e6be01e72b90ba715bbad7c67afa5bb5
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-05-14 09:53:05 -0300

    Add files via upload

---
+++ Lista2ex1/Lista2ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro valor: ");
+        int valor1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo valor: ");
+        int valor2 = int.Parse(Console.ReadLine());
+
+        if (valor1 > valor2)
+        {
+            Console.Write("O maior valor é: " + valor1);
+        }
+        else if (valor2 > valor1)
+        {
+            Console.Write("O maior valor é: " + valor2);
+        }
+        else
+        {
+            Console.WriteLine("Os valores são iguais.");
+        }
+    }
+}
